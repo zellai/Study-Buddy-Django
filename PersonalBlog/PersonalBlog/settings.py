5530f904528080838f770d9695b8e44128c4e20c
Ezel Espera <zellai022890@gmail.com>
2023-10-24 15:53:53 +0800

    Landing Page with NavBar

--- PersonalBlog/PersonalBlog/settings.py
+++ PersonalBlog/PersonalBlog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'PersonalBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
