534538b1073529a39fee4fae6edf8f615a17fe93
Ezel Espera <zellai022890@gmail.com>
2023-10-24 17:30:23 +0800

    Add Template and make migration

--- PersonalBlog/base/views.py
+++ PersonalBlog/base/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+rooms= [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Fontend developers'},
+]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}        
+    return render(request, 'base/room.html', context)
