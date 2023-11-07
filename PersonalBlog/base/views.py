dd0ab7c922de4032ead79df91c79d69449208fae
Ezel Espera <zellai022890@gmail.com>
2023-11-07 13:36:49 +0800

    Rest Framework

--- PersonalBlog/base/views.py
+++ PersonalBlog/base/views.py
@@ -191,4 +191,4 @@ def topicsPage(request):
 
 def activityPage(request):
     room_messages = Message.objects.all()
-    return render(request, 'base/activity.html', {'room_messages': room_messages})
+    return render(request, 'base/activity.html', {'room_messages': room_messages})  
